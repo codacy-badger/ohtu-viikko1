324c540a85ea24fcac141ada7f77c64bb36d7935
Aleksi Saarela <aleksi-i@windowslive.com>
2018-10-28 00:23:27 +0300

    korjaa rikotun testin

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -118,7 +118,7 @@ public class VarastoTest {
 
     @Test
     public void tuottaaMerkkijonoEsityksen() {
-        String expected = "saldo = 0.0, vielä tilaa 10.0 asd";
+        String expected = "saldo = 0.0, vielä tilaa 10.0";
         assertEquals(expected, varasto.toString());
     }
 }
